d4deafe1c51e34de177728e1d8310a162dad6e1f
Tanmoy Roy <tanmoyroy99@gmail.com>
2018-12-15 11:48:32 +0530

    first commit.

---
+++ index.js
@@ -0,0 +1,59 @@
+require('express-async-errors');
+const winston = require('winston');
+const error = require('./middleware/error');
+var express = require("express");
+var app = express();
+const genres = require('./routes/genres');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const day2day = require('./routes/day2day');
+//const dbconnection = require('./models/dbconnection');
+const mongoose = require('mongoose');
+
+
+
+winston.handleExceptions(
+	new winston.transports.File({filename: 'uncaughtException.log'})
+);
+
+process.on('unhandledRejection', (ex) => {
+	throw ex;
+});
+
+
+
+winston.add(winston.transports.File, { filename: 'logfile.log' }); 
+
+/* mongodb://tanmoy:tanmoy@127.0.0.1:27017/dressbook */ //----------AWS
+/* mongodb://tanmoyroy:tanmoyroy123@ds149144.mlab.com:49144/daytwoday */ //----------MLAB tanmoyroy99@gmail.com
+
+
+
+mongoose.connect('mongodb://tanmoyroy:tanmoyroy123@ds149144.mlab.com:49144/daytwoday')
+.then(()=> {console.log('DB connected...');
+	next();
+})
+.catch(err=> {
+	console.error(err);
+});
+
+/* -------------------------------------------------------------------------------------------------- */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+  next();
+});
+/* -------------------------------------------------------------------------------------------------- */
+
+app.use(express.json());
+app.use('/api/genres',  genres);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/day2day', day2day);
+app.use(error);
+
+
+
+app.listen(8080,function(){
+    console.log('Express app start on port.. 8080')
+});
